8218c84ac3820ecfe3f66e49fa96587e208cd46f
Dorothy-Allie-Hong <imdorothy3@gmail.com>
2022-11-15 01:38:46 +0900

    게시판 목록 생성

---
+++ notice.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://zoon:1234@cluster0.dbul0bg.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.hotdogs
+
+@app.route('/')
+def home():
+   return render_template('notice.html')
+
+@app.route("/hotdog-list", methods=["GET"])
+def hotdog_get():
+    hotdog_list = list(db.menu.find({}, {'_id': False}))
+    return jsonify({'hotdogs':hotdog_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
