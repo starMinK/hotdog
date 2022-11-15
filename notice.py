519ed85a00fe86cfa135ea60d0c7f793dfeab0fc
Dorothy-Allie-Hong <imdorothy3@gmail.com>
2022-11-15 16:19:07 +0900

    확인용 커밋

--- notice.py
+++ notice.py
@@ -14,5 +14,9 @@ def hotdog_get():
     hotdog_list = list(db.menu.find({}, {'_id': False}))
     return jsonify({'hotdogs':hotdog_list})
 
+@app.route("/detail")
+def detail():
+    return render_template("detail.html")
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
