007ed12b2afc524af45f1dc9f36176dbb3ead05e
junyoungNA <nanasooo@naver.com>
2022-11-15 17:35:05 +0900

    Add python파일 수정 메인페이지 질문 기능 구현

--- zoon.py
+++ zoon.py
@@ -18,9 +18,10 @@ def menu_get():
     menu_list = list(db.menu.find({}, {'_id': False}))
     return jsonify({'menu_list': menu_list})
 
-@app.route("/recommend", methods=["GET"])
-def recommend_get():
-    return jsonify({'recommend':"recommend요청"})
+@app.route("/recomment", methods=["GET"])
+def recomment():
+    return render_template('recomment.html');
+
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5500, debug=True)
